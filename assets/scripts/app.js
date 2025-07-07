cdd77407a4ff04b9f117812cec49a8945ca5abf4
domizalewska <107934603+domizalewska@users.noreply.github.com>
2025-07-07 19:11:56 +0200

    feat: add test

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,1 +1,37 @@
-alert('This works');
+
+
+function getUserNumberInput() {
+    return parseInt(getUserNumberInput.value);
+}
+
+function createAndwriteOutput(operator, resultBeforeCalc, calcNumber)
+{
+    const calcDescription = `${resultBeforeCalc} ${operator} ${calcNumber}`;
+    outputResult(currentResult,calcDescription);
+}
+
+function writeToLog(
+operationIdentifier,
+prevResult,
+operationNumber,
+newResult
+){
+    const logEntry = {
+        operation: "Add",
+        prevResult: initialResult,
+        number: enteredNumber,
+        result: currentResult
+    };
+    logEntries.push(logEntry);
+    console.log(logEntries);
+}
+
+
+function add() 
+{
+    const enteredNumber = getUserNumberInput();
+    const initialResult = enteredNumber;
+    currentResult += enteredNumber;
+    createAndwriteOutput('+', initialResult, enteredNumber);
+    
+}
