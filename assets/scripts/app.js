c93d102264aaa37da5b79e629995094ae1193086
domizalewska <107934603+domizalewska@users.noreply.github.com>
2025-07-07 22:00:43 +0200

    Feat: clickable calculator

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,37 +1,49 @@
+const defaultResult = 0;
+let currentResult = defaultResult;
+let logEntries = [];
 
-
+// Gets input from input field
 function getUserNumberInput() {
-    return parseInt(getUserNumberInput.value);
+  return parseInt(usrInput.value);
 }
 
-function createAndwriteOutput(operator, resultBeforeCalc, calcNumber)
-{
-    const calcDescription = `${resultBeforeCalc} ${operator} ${calcNumber}`;
-    outputResult(currentResult,calcDescription);
+// Generates and writes calculation log
+function createAndWriteOutput(operator, resultBeforeCalc, calcNumber) {
+  const calcDescription = `${resultBeforeCalc} ${operator} ${calcNumber}`;
+  outputResult(currentResult, calcDescription); // from vendor file
 }
 
-function writeToLog(
-operationIdentifier,
-prevResult,
-operationNumber,
-newResult
-){
-    const logEntry = {
-        operation: "Add",
-        prevResult: initialResult,
-        number: enteredNumber,
-        result: currentResult
-    };
-    logEntries.push(logEntry);
-    console.log(logEntries);
+function add() {
+  const enteredNumber = getUserNumberInput();
+  const initialResult = currentResult;
+  currentResult += enteredNumber;
+  createAndWriteOutput('+', initialResult, enteredNumber);
+  logEntries.push(enteredNumber);
+  console.log(logEntries[1]);
 }
 
-
-function add() 
-{
-    const enteredNumber = getUserNumberInput();
-    const initialResult = enteredNumber;
-    currentResult += enteredNumber;
-    createAndwriteOutput('+', initialResult, enteredNumber);
-    
+function subtract() {
+  const enteredNumber = getUserNumberInput();
+  const initialResult = currentResult;
+  currentResult -= enteredNumber;
+  createAndWriteOutput('-', initialResult, enteredNumber);
 }
+
+function multiply() {
+  const enteredNumber = getUserNumberInput();
+  const initialResult = currentResult;
+  currentResult *= enteredNumber;
+  createAndWriteOutput('*', initialResult, enteredNumber);
+}
+
+function divide() {
+  const enteredNumber = getUserNumberInput();
+  const initialResult = currentResult;
+  currentResult /= enteredNumber;
+  createAndWriteOutput('/', initialResult, enteredNumber);
+}
+
+addBtn.addEventListener('click', add);
+subtractBtn.addEventListener('click', subtract);
+multiplyBtn.addEventListener('click', multiply);
+divideBtn.addEventListener('click', divide);
